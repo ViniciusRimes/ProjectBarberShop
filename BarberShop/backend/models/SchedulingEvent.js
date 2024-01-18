11a3698d149d0e3b1ceaeedc2f619fba0660f558
ViniciusRimes <viniciusrimess@gmail.com>
2024-01-18 17:23:20 -0300

    scheduling functions

--- BarberShop/backend/models/SchedulingEvent.js
+++ BarberShop/backend/models/SchedulingEvent.js
@@ -1,12 +1,40 @@
 const {DataTypes} = require('sequelize')
 const conn = require('../db/conn')
-
-const SchedulingEvent = conn.define('SchedulingEvent',{
-
-})
-
 const Client = require('./Client')
 const Scheduling = require('./Scheduling')
-Client.belongsToMany(Scheduling, {through: SchedulingEvent})
-Scheduling.belongsToMany(Client, {through: SchedulingEvent})
+const Services = require('./Services')
+
+const SchedulingEvent = conn.define('SchedulingEvent',{
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    ClientId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    SchedulingId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Scheduling,
+            key: 'id',
+        },
+        allowNull: false,
+    },
+    ServiceId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Services,
+            key: 'id',
+        },
+        allowNull: false,
+    },
+})
+
+Client.belongsToMany(Scheduling, { through: SchedulingEvent });
+Scheduling.belongsToMany(Client, { through: SchedulingEvent });
+Services.belongsToMany(Client, { through: SchedulingEvent });
+Services.belongsToMany(Scheduling, { through: SchedulingEvent });
+
 module.exports = SchedulingEvent
