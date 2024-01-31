453ad56cf6dbdbee0edcc5af31d7667986387088
ViniciusRimes <viniciusrimess@gmail.com>
2024-01-30 21:10:44 -0300

    fixing backend bugs

--- BarberShop/backend/controllers/BarberShopController.js
+++ BarberShop/backend/controllers/BarberShopController.js
@@ -97,8 +97,11 @@ module.exports = class BarberShopController{
     static async getUser(req, res){
         try{
             const user = await getBarberShop(req, res)
+            if(!user){
+                return
+            }
             user.password = ''
-            res.status(200).json({message: {user: user }})
+            res.status(200).send(user)
         }catch(error){
             res.status(400).json({message: "Erro: " + error})
         }
