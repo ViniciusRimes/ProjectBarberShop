40550659dff83230272963b327c1590d9d49d7e3
ViniciusRimes <viniciusrimess@gmail.com>
2024-01-30 21:13:02 -0300

    components of message using EventEmitter

---
+++ BarberShop/frontend/BarberShop/src/components/layout/Messages.jsx
@@ -0,0 +1,30 @@
+import styles from './Messages.module.css'
+import { useState, useEffect } from 'react'
+import bus from '../../helpers/bus'
+
+const Messages = () => {
+    const [visibility, setVisibility] = useState(false)
+    const [type, setType] = useState("")
+    const [message, setMessage] = useState("")
+
+    useEffect(()=>{
+        bus.addListener('flash', ({message, type}) =>{
+            setVisibility(true)
+            setMessage(message)
+            setType(type)
+
+            setTimeout(() => {
+                setVisibility(false)
+            }, 4000);
+        })
+    }, [])
+
+    return (
+        visibility &&
+        <div className={`${styles.message} ${styles[type]}`}>
+            {message}
+        </div>
+  )
+}
+
+export default Messages
